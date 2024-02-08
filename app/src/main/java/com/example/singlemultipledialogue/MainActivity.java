47e146338daa0286332585cd51a2d65b5b11d490
RealGuzu <45natewilson@gmail.com>
2024-02-08 09:49:12 -0500

    Final

--- app/src/main/java/com/example/singlemultipledialogue/MainActivity.java
+++ app/src/main/java/com/example/singlemultipledialogue/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.singlemultipledialogue;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     TextView txt;
 
     CharSequence[] items = {"Pizza", "Burger", "Sushi", "Tacos", "Pasta"};
+    boolean[] selectedItems = {false, false, false, false, false};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,27 +25,70 @@ public class MainActivity extends AppCompatActivity {
 
         txt = findViewById(R.id.displayTXT);
         btnSingle = findViewById(R.id.singleChoiceButton);
-
+        btnMulti = findViewById(R.id.multiChoiceButton);
 
         // Moved the setting of text after initializing txt
         txt.setText(items[0]);
 
-        btnSingle.setOnClickListener(v -> {
-            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
-            alertDialogBuilder.setTitle("Select Food");
-            alertDialogBuilder.setCancelable(true);
-            alertDialogBuilder.setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    txt.setText(items[which]);
-                    dialog.dismiss();
-                }
-            });
-            AlertDialog alertDialog = alertDialogBuilder.create();
-            alertDialog.setCanceledOnTouchOutside(true);
-            alertDialog.show();
+        btnSingle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSingleChoiceDialog();
+            }
         });
 
+        btnMulti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showMultiChoiceDialog();
+            }
+        });
+    }
 
+    private void showSingleChoiceDialog() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        alertDialogBuilder.setTitle("Select Food");
+        alertDialogBuilder.setCancelable(true);
+        alertDialogBuilder.setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                txt.setText(items[which]);
+                dialog.dismiss();
+            }
+        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.setCanceledOnTouchOutside(true);
+        alertDialog.show();
+    }
+
+    private void showMultiChoiceDialog() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        alertDialogBuilder.setTitle("Select Food");
+        alertDialogBuilder.setCancelable(true);
+        alertDialogBuilder.setMultiChoiceItems(items, selectedItems, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                selectedItems[which] = isChecked;
+            }
+        });
+        alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                displaySelectedItems();
+            }
+        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.setCanceledOnTouchOutside(true);
+        alertDialog.show();
+    }
+
+    private void displaySelectedItems() {
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < selectedItems.length; i++) {
+            if (selectedItems[i]) {
+                text.append(items[i]).append(" ");
+            }
+        }
+        txt.setText(text.toString().trim());
     }
 }
--- app/src/main/java/com/example/singlemultipledialogue/Multi.java
+++ app/src/main/java/com/example/singlemultipledialogue/Multi.java
@@ -1,4 +1,59 @@
-package com.example.singlemultipledialogue;
-
-public class Multi {
-}
+//package com.example.singlemultipledialogue;
+//
+//import android.content.DialogInterface;
+//import android.os.Bundle;
+//import android.widget.Button;
+//import android.widget.TextView;
+//
+//import androidx.appcompat.app.AlertDialog;
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//public class Multi extends AppCompatActivity {
+//
+//    Button btn;
+//    TextView display;
+//
+//    CharSequence[] items = {"Pizza", "Burger", "Sushi", "Tacos", "Pasta"};
+//
+//    boolean[] selectedItems = {false, false, false, false, false};
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        btn = findViewById(R.id.multiChoiceButton);
+//        display = findViewById(R.id.displayTXT);
+//
+//        btn.setOnClickListener(v -> {
+//            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(Multi.this);
+//            alertDialogBuilder.setTitle("Select Food");
+//            alertDialogBuilder.setCancelable(true);
+//            alertDialogBuilder.setMultiChoiceItems(items, selectedItems, new DialogInterface.OnMultiChoiceClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+//                    selectedItems[which] = isChecked;
+//                }
+//            });
+//            alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                    display.setText(itemsToString());
+//                }
+//            });
+//            AlertDialog alertDialog = alertDialogBuilder.create();
+//            alertDialog.setCanceledOnTouchOutside(true);
+//            alertDialog.show();
+//        });
+//    }
+//
+//    private String itemsToString() {
+//        StringBuilder text = new StringBuilder();
+//        for (int i = 0; i < selectedItems.length; i++) {
+//            if (selectedItems[i]) {
+//                text.append(items[i]).append(" ");
+//            }
+//        }
+//        return text.toString().trim();
+//    }
+//}
